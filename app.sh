4da592b03838e2074d3ba2010bbb05f6892e30f1
Curtis Lacy <curtis@engine.co>
2014-02-25 21:17:26 +0000

    Move the MSC parsing workspace into /var/lib/omniwallet/msc, to make room for other parsing loops.

--- app.sh
+++ app.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 kill_child_processes() {
+	echo "KILLING $SERVER_PID"
 	kill $SERVER_PID
-	rm -f $LOCK_FILE
+	echo "KILLING $MSC_LOOP_PID"
+	kill $MSC_LOOP_PID
 }
 
 # Ctrl-C trap. Catches INT signal
@@ -11,14 +13,10 @@ trap "kill_child_processes 1 $$; exit 0" INT
 APPDIR=`pwd`
 TOOLSDIR=$APPDIR/node_modules/mastercoin-tools
 DATADIR="/var/lib/omniwallet"
-LOCK_FILE=$DATADIR/msc_cron.lock
-PARSE_LOG=$DATADIR/parsed.log
-VALIDATE_LOG=$DATADIR/validated.log
-ARCHIVE_LOG=$DATADIR/archived.log
 
-if [ ! -d $DATADIR/tx ]; then
-	cp -r $TOOLSDIR/www/tx-bootstrap $DATADIR/tx
-fi
+$APPDIR/bin/parse-msc.sh &
+MSC_LOOP_PID=$!
+echo "MSC Parse Loop PID: $MSC_LOOP_PID"
 
 # Export directories for API scripts to use
 export TOOLSDIR
@@ -29,44 +27,8 @@ SERVER_PID=$!
 
 while true
 do
-
-	# check lock (not to run multiple times)
-	if [ ! -f $LOCK_FILE ]; then
-
-		# lock
-		touch $LOCK_FILE
-
-		mkdir -p $DATADIR
-		cd $DATADIR
-		mkdir -p tmptx tx addr general offers wallets mastercoin_verify/addresses mastercoin_verify/transactions www
-
-		# parse until full success
-		x=1 # assume failure
-		echo -n > $PARSE_LOG
-		while [ "$x" != "0" ];
-		do
-			python $TOOLSDIR/msc_parse.py -r $TOOLSDIR 2>&1 >> $PARSE_LOG
-  			x=$?
-		done
-
-		python $TOOLSDIR/msc_validate.py 2>&1 > $VALIDATE_LOG
-	
-		# update archive
-		python $TOOLSDIR/msc_archive.py -r $TOOLSDIR 2>&1 > $ARCHIVE_LOG
-	
-		mkdir -p $DATADIR/www/tx $DATADIR/www/addr $DATADIR/www/general $DATADIR/www/offers $DATADIR/www/mastercoin_verify/addresses $DATADIR/www/mastercoin_verify/transactions
-
-        cp $DATADIR/tx/* $DATADIR/www/tx
-		cp $DATADIR/addr/* $DATADIR/www/addr
-		cp $DATADIR/general/* $DATADIR/www/general
-		cp $DATADIR/offers/* $DATADIR/www/offers
-		cp $DATADIR/mastercoin_verify/addresses/* $DATADIR/www/mastercoin_verify/addresses
-		cp $DATADIR/mastercoin_verify/transactions/* $DATADIR/www/mastercoin_verify/transactions
-	
-		# unlock
-		rm -f $LOCK_FILE
-	fi
-
-	# Wait a minute, and do it all again.
-	sleep 60
+	# Sleep a really long time each loop - this is really just to keep the process from exiting.
+	# and so that we know we have something to kill with ctrl-c to brinf everything else down.
+	sleep 3600
 done
+
---
+++ bin/parse-msc.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+echo "SPAWNED SUBPROCESS!"
+
+remove_lock() {
+	echo "SUBPROCESS TRAP!"
+	rm -f $LOCK_FILE
+}
+
+# Ctrl-C trap. Catches INT signal
+trap "remove_lock 1 $$; exit 0" EXIT
+
+APPDIR=`pwd`
+TOOLSDIR=$APPDIR/node_modules/mastercoin-tools
+DATADIR="/var/lib/omniwallet/msc"
+LOCK_FILE=$DATADIR/msc_cron.lock
+PARSE_LOG=$DATADIR/parsed.log
+VALIDATE_LOG=$DATADIR/validated.log
+ARCHIVE_LOG=$DATADIR/archived.log
+
+if [ ! -d $DATADIR/tx ]; then
+	cp -r $TOOLSDIR/www/tx-bootstrap $DATADIR/tx
+fi
+
+while true
+do
+
+	# check lock (not to run multiple times)
+	if [ ! -f $LOCK_FILE ]; then
+
+		# lock
+		touch $LOCK_FILE
+
+		mkdir -p $DATADIR
+		cd $DATADIR
+		mkdir -p tmptx tx addr general offers wallets mastercoin_verify/addresses mastercoin_verify/transactions www
+
+		# parse until full success
+		x=1 # assume failure
+		echo -n > $PARSE_LOG
+		while [ "$x" != "0" ];
+		do
+			python $TOOLSDIR/msc_parse.py -r $TOOLSDIR 2>&1 >> $PARSE_LOG
+  			x=$?
+		done
+
+		python $TOOLSDIR/msc_validate.py 2>&1 > $VALIDATE_LOG
+	
+		# update archive
+		python $TOOLSDIR/msc_archive.py -r $TOOLSDIR 2>&1 > $ARCHIVE_LOG
+	
+		mkdir -p $DATADIR/www/tx $DATADIR/www/addr $DATADIR/www/general $DATADIR/www/offers $DATADIR/www/mastercoin_verify/addresses $DATADIR/www/mastercoin_verify/transactions
+
+	        cp $DATADIR/tx/* $DATADIR/www/tx
+		cp $DATADIR/addr/* $DATADIR/www/addr
+		cp $DATADIR/general/* $DATADIR/www/general
+		cp $DATADIR/offers/* $DATADIR/www/offers
+		cp $DATADIR/mastercoin_verify/addresses/* $DATADIR/www/mastercoin_verify/addresses
+		cp $DATADIR/mastercoin_verify/transactions/* $DATADIR/www/mastercoin_verify/transactions
+	
+		# unlock
+		rm -f $LOCK_FILE
+	fi
+
+	# Wait a minute, and do it all again.
+	sleep 60
+done
